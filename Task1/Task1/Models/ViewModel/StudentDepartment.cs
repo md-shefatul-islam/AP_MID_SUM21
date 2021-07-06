f58bffb432d6223305ee99eb04f306c32df7b949
shefat <shefatul.islam99@gmail.com>
2021-07-07 01:39:26 +0600

    mid task

---
+++ Task1/Task1/Models/ViewModel/StudentDepartment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task1.Models.ViewModel
+{
+    public class StudentDepartment
+    {
+        [Required(ErrorMessage = "Please enter your dept")]
+        public int Dept_Id { get; set; }
+        [Required(ErrorMessage = "Please enter your name here")]
+        [MaxLength(30, ErrorMessage = "Max Length 30")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter DATE OF BIRTH")]
+        public string DOB { get; set; }
+        [Required]
+        [Range(1, 148)]
+        public int Credit { get; set; }
+        [Required]
+        public double CGPA { get; set; }
+
+        public Department Department { get; set; }
+        public Student Student { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<Student> Students { get; set; }
+        public StudentDepartment()
+        {
+
+            List<Department> Departments = new List<Department>();
+        }
+    }
+}
